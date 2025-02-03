7737fe48ad2d0b75da914ac197af9487ebe81fdb
Rafael <59889940+rmmatos87@users.noreply.github.com>
2025-02-03 03:39:53 -0300

    Add files via upload

---
+++ exe/run_mobicity.py
@@ -0,0 +1,194 @@
+from getpass import getpass
+from mobicity import Mobicity
+from os import listdir
+from os.path import join, split
+
+from datetime import datetime, timedelta
+import json
+import sys
+
+_hoje = datetime.today().strftime("%d/%m/%Y")
+_help = f"""Seguem parâmetros, inclusive o que é visto será considerado como padrão, caso não informado.
+--usuario=[raiz do e-mail] : Parte anterior ao @petrobras.com.br
+--json=parameters.json : Definição de arquivo onde buscar parâmetros
+--turno=dia : [dia/noite]
+--inicio={_hoje} : Definição de data de início do turno
+--dias=6 : Definição de dias do turno
+"""
+
+def busca_json(filename=None):
+    dir = split(__file__)[0]
+    jsons = [i for i in listdir(dir) if ".json" in i]
+    if filename is not None:
+        if filename not in jsons:
+            raise FileExistsError(f"Arquivo '{filename}' não encontrado.")
+    else:
+        if len(jsons) > 1:
+            print("Selecione o arquivo:")
+            for i, file in enumerate(jsons):
+                print(i+1, "-", file)
+            n = int(input("Número do arquivo: "))-1
+            jsonfile = jsons[n]
+        else:
+            jsonfile = jsons[0]
+        return join(dir, jsonfile)
+
+def le_json(jsonpath: str):
+    with open(jsonpath, encoding="utf-8") as f:
+        txt = f.read()
+        data = json.loads(txt)
+    return data
+
+def setup(m, usr, pwd, k=2):
+    for i in range(k):
+        print("Executando!")
+        m.setup_rides(usr, pwd, verbose=True)
+
+def configura_mobicity(data: dict):
+    year = datetime.today().year
+
+    addresses = data['addresses']
+    shift = ["day", "night", "3d+3n"][int(input('Dia [1] / Noite [2] / 3D+3N [3]: ')) - 1]
+    start_day = input("Data de início [DD/MM/AAAA ou DD/MM/*ano atual*]: ")
+
+    start_day = start_day if len(start_day.split(r"/")) == 3 else start_day + r"/" + str(year)
+
+    m = False
+    m1 = False
+    m2 = False
+    if shift in ["day", "night"]:
+        time_to_work = data["time"]["time_to_work"][shift]
+        time_to_home = data["time"]["time_to_home"][shift]
+        d = input("Quantos dias da escala? [1 a 6], 6 é a resposta padrão ")
+        d = int(d) if d in map(str, range(1,7)) else 6
+
+        m = Mobicity(
+            shift=shift,
+            time_to_work=time_to_work,
+            time_to_home=time_to_home,
+            start_day=start_day,
+            days=d,
+            **addresses
+        )
+    else:
+        #Armengue para escala 3D+3N
+        # parte 1: Dia
+        time_to_work = data["time"]["time_to_work"]["day"]
+        time_to_home = data["time"]["time_to_home"]["day"]
+        m1 = Mobicity(
+            shift="day",
+            time_to_work=time_to_work,
+            time_to_home=time_to_home,
+            start_day=start_day,
+            days=3,
+            **addresses
+        )
+        
+        # parte 2: Noite
+        start_day = (datetime.fromisoformat("-".join(start_day.split(r"/")[::-1])) + timedelta(days=3)).strftime(r"%d/%m/%Y")
+        time_to_work = data["time"]["time_to_work"]["night"]
+        time_to_home = data["time"]["time_to_home"]["night"]
+        m2 = Mobicity(
+            shift="night",
+            time_to_work=time_to_work,
+            time_to_home=time_to_home,
+            start_day=start_day,
+            days=3,
+            **addresses
+        )
+
+    pwd = getpass("Senha do Mobicity (não é a senha Petrobras): ")
+    for mx in [m, m1, m2]:
+        if mx:
+            mx.browser_name = "edge"
+            for schedule in data['week_schedule'][mx.shift]:
+                mx.setup_schedule_weekdays(**schedule)
+            print(mx)
+
+            if input("Confirma parâmetros? ([S]/N) ") in ["Sim", "S", "s", "", " "]:
+                setup(
+                    mx,
+                    data['username'] + "@petrobras.com.br",
+                    pwd
+                )
+
+if "--help" in sys.argv:
+    print(_help)
+else:
+    argv = {x[0].replace("--", ""): x[1] for x in [i.split("=") for i in sys.argv[1:] if "=" in i]}
+    if "json" not in argv:
+        argv["json"] = busca_json()
+    
+    data = le_json(argv["json"])
+
+    configura_mobicity(data)
+
+if "--rafael" in sys.argv:
+    addresses = {
+        "home": "R. Araguaia, 1613 - Freguesia de Jacarepaguá, Rio de Janeiro - RJ, 22745-271, Brasil",
+        "work_front": "Av. Henrique3 Valadares, 26 - Lapa, Rio de Janeiro - RJ, 20231-030, Brazil",
+        "work_back": "R. do Senado, 115 - Centro, Rio de Janeiro - RJ, 20231-000, Brazil",
+        "fair": "R. Joaquim Pinheiro, 16 - Freguesia de Jacarepaguá, Rio de Janeiro - RJ, 22743-660, Brazil",
+        "crossfit": "R. Araguaia, 895 - Freguesia de Jacarepaguá, Rio de Janeiro - RJ, 22745-270, Brasil"
+    }
+
+    shift = "day" if argv.get("turno", "day").lower()=="dia" else "night"
+
+    time_to_work = {"day": "05:55",
+                    "night": "17:55"}[shift]
+    time_to_home = {"day": "19:05",
+                    "night": "07:05"}[shift]
+    
+    m = Mobicity(
+        shift=shift,
+        time_to_work=time_to_work,
+        time_to_home=time_to_home,
+        start_day="01/11/2024",
+        days=6,
+        **addresses
+    )
+    m.browser_name = "edge"
+
+    # Inserindo exceções por dia da semana
+    if m.shift == "day":
+        m.setup_schedule_weekdays("to_work", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+        m.setup_schedule_weekdays("to_home", [0, 1, 2, 3, 4], home="crossfit", work="work_back")
+        m.setup_schedule_weekdays("to_home", [5, 6], home="home", work="work_front")
+    else:
+        m.setup_schedule_weekdays("to_work", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+        m.setup_schedule_weekdays("to_home", [0, 1, 2, 3, 4], home="home", work="work_back")
+        m.setup_schedule_weekdays("to_home", [5, 6], home="home", work="work_front")
+    print(m)
+    m.setup_rides(
+        "rafael_matos@petrobras.com.br",
+        getpass("Senha do Mobicity (não é a senha Petrobras): ")
+    )
+
+    # addresses = {
+    #     "home": "Rua Barão de Ipanema, 29. Copacabana. Rio de Janeiro",
+    #     "work_front": "Av. Henrique Valadares, 28 - Lapa, Rio de Janeiro - RJ, 20231-030, Brazil",
+    #     "work_back": "R. do Senado, 115 - Centro, Rio de Janeiro - RJ, 20231-000, Brazil",
+    #     "fair": "R. Joaquim Pinheiro, 16 - Freguesia de Jacarepaguá, Rio de Janeiro - RJ, 22743-660, Brazil",
+    #     "crossfit": "R. Araguaia, 895 - Freguesia de Jacarepaguá, Rio de Janeiro - RJ, 22745-270, Brasil"
+    # }
+    # shift = ["day", "night"][1]
+    # time_to_work = {"day": "06:25",
+    #                 "night": "18:15"}[shift]
+    # time_to_home = {"day": "19:30",
+    #                 "night": "07:30"}[shift]
+    # m = Mobicity(
+    #     shift=shift,
+    #     time_to_work=time_to_work,
+    #     time_to_home=time_to_home,
+    #     start_day="02/10/2024",
+    #     days=6,
+    #     **addresses
+    # )
+    # m.browser_name = "edge"
+    # if m.shift == "day":
+    #     m.setup_schedule_weekdays("to_work", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+    #     m.setup_schedule_weekdays("to_home", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+    # else:
+    #     m.setup_schedule_weekdays("to_work", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+    #     m.setup_schedule_weekdays("to_home", [0, 1, 2, 3, 4, 5, 6], home="home", work="work_front")
+    # m.setup_rides("arraesff@petrobras.com.br", getpass("Senha do Mobicity (não é a senha Petrobras): "))
